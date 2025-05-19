0aacc2c57238af75675d5180be9674fe5c3b6379
rick672 <aliagaricardo321@gmail.com>
2025-05-19 12:12:45 -0400

    Autentication Google

--- src/domain/use.cases/auth/loginEmailPassword.use_cases.ts
+++ src/domain/use.cases/auth/loginEmailPassword.use_cases.ts
@@ -5,7 +5,7 @@ import type { UserCredential } from 'firebase/auth'
 export class LoginEmailPasswordUseCases {
     constructor(private readonly authService: AuthService) {}
 
-    static execute(email: string, password: string): Promise<UserCredential> {
+    static execute(email: string, password: string): Promise<UserCredential | undefined> {
         return new AuthService(new AuthModel()).signInEmailPassword(email, password)
     }
 }
